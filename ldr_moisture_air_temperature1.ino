523595d6e919f77ef7806dec7bc35051dc8668d6
Tomas <57348340+Charmeister11@users.noreply.github.com>
2022-10-14 15:18:37 +0200

    added normalization for humidity

--- ldr_moisture_air_temperature1.ino
+++ ldr_moisture_air_temperature1.ino
@@ -26,11 +26,15 @@ void loop()
   digitalWrite(A0, HIGH);
   delay(10); // Wait for 10 millisecond(s)
   moisture = analogRead(A1);
+  moisture = floor(10000*((moisture - MoistureMin) / (MoistureMax - MoistureMin)) * 100)/10000;
   // Turn off the sensor to reduce metal corrosion
   // over time
   digitalWrite(A0, LOW);
   Serial.print("Moisture = ");
-  Serial.println(moisture);
+  Serial.print(moisture);
+  Serial.print(" %");
+  Serial.println();
+               
   delay(1000); // Wait for 500 millisecond(s)
   
   Light = analogRead(A2);
