454f939428a15114d2875add7d2d93f6931629c8
Tomas <57348340+Charmeister11@users.noreply.github.com>
2022-10-14 15:20:30 +0200

    Update ldr_moisture_air_temperature1.ino

--- ldr_moisture_air_temperature1.ino
+++ ldr_moisture_air_temperature1.ino
@@ -1,5 +1,8 @@
 // C++ code
 //
+#include<math.h>
+static int MOISTURE_MIN = 234
+static int MOISTURE_MAX = 1008
 int moisture = 0;
 
 int Light = 0;
@@ -26,7 +29,7 @@ void loop()
   digitalWrite(A0, HIGH);
   delay(10); // Wait for 10 millisecond(s)
   moisture = analogRead(A1);
-  moisture = floor(10000*((moisture - MoistureMin) / (MoistureMax - MoistureMin)) * 100)/10000;
+  moisture = floor(10000*((moisture - MOISTURE_MIN) / (MOISTURE_MAX - MOISTURE_MIN)) * 100)/10000;
   // Turn off the sensor to reduce metal corrosion
   // over time
   digitalWrite(A0, LOW);
